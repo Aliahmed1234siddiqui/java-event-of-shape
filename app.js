b6049cdfcfba207b1080cb94f93f1fa27f44124c
Aliahmed1234siddiqui <aliahmedsiddiqui48@gmail.com>
2023-05-27 00:22:28 +0500

    Done

---
+++ app.js
@@ -0,0 +1,17 @@
+var main = document.getElementById("main");
+
+ function circle(){
+ main.className = "main2";
+
+  }
+
+ function rect(){
+
+    main.className = "main";
+ }
+ function square(){
+    main.className = "main3";
+ }
+ function tri (){
+    main.className = "main4"; 
+ }
